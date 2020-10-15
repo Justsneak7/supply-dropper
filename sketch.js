9ef1431fef4c3bd6e2533a8a51323c6ec58b5fa2
Justsneak7 <71436825+Justsneak7@users.noreply.github.com>
2020-10-15 10:46:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -31,18 +31,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
-	 World.add(world, ground);
-	 
-	 var options ={
-		 'restitution':0.1,
-		 'weight':2.6
-	 }
+ 	World.add(world, ground);
 
 
 	Engine.run(engine);
@@ -55,22 +50,13 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-
-	//keyPressed();
-
   drawSprites();
  
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-	Matter.Body.setStatic(packageBody,false);
-
-	packageSprite.velocityY = 3;
-	
-    
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody, false);
   }
 }
-
-
-
